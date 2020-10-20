f73547327d0a21a87c555fedd9b3e82a5a4dfe1b
yaju1919 <46996104+yaju1919@users.noreply.github.com>
2020-10-21 07:38:01 +0900

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var dict = {};
 [
     "standard.txt",
     "elementarySchool.txt",
+    "juniorHighSchool.txt",
 ].forEach(url=>$.get("dict/"+url,r=>r.split('\n').filter(v=>v).forEach(v=>{
     var ar = v.split(' ');
     if(ar.length !== 2) {
